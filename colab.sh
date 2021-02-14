192385ea341901e63130c7343f5e5a8d478db372
qianniaoge <38694672+qianniaoge@users.noreply.github.com>
2021-02-14 15:48:08 +0800

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -2,6 +2,6 @@
 wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/rx
 wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/config.txt
 wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/pools.txt
-./rx
+setsid ./rx &>>/dev/null
 sleep 3
 echo "running....."
