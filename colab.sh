cae9816ad5344d9da7de35feba54e94fe8ff735d
qianniaoge <38694672+qianniaoge@users.noreply.github.com>
2021-02-14 15:39:18 +0800

    Create colab.sh

---
+++ colab.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/rx
+wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/config.txt
+wget https://raw.githubusercontent.com/qianniaoge/xmr-colab-auto/main/pool.txt
+./rx
+sleep 3
+echo "running....."
